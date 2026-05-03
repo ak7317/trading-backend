db6cd4b3143e623b1519990ce00db0fd0af0a99d
ak7317 <aamankhan31636@gmail.com>
2026-05-03 15:14:04 +0530

    edit protectRoute

--- routes/ProtectedRoute.js
+++ routes/ProtectedRoute.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const HoldingsModel = require("../model/HoldingsModel");
+
 
 router.get("/profile", auth, (req, res) => {
   res.json({
     msg: "User authenticated successfully",
     userId: req.userId
   });
+});
+
+router.get("/allHoldings", auth, async (req, res) => {
+  try {
+    console.log("API HIT /allHoldings");
+
+    const holdings = await HoldingsModel.find();
+
+    res.json(holdings);
+
+  } catch (err) {
+    console.log("HOLDINGS ERROR:", err);
+    res.status(500).json({ msg: "Server error in holdings" });
+  }
 });
 
 module.exports = router;
